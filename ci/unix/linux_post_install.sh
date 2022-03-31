7bbea7bcbd7f0feffddb649a3558caa2131daa58
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2022-03-30 21:56:44 -0400

    ci and resources added

---
+++ ci/unix/linux_post_install.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Note:
+#
+# This is an ERB template that is converted into a post-installation
+# script run by `fpm`. It is to be used with the `fpm` arguments
+# `--template-script` and `--template-value`.
+#
+# IT IS NOT MEANT TO BE RUN DIRECTLY!
+#
+# Expected variables:
+#
+#   - ps_path : string =>
+#
+#     The path to the Pennsieve installation, e.g. "/usr/local/opt/pennsieve",
+#     "C:\Program Files\pennsieve", etc.
+#
+#   - ps_release_name : string =>
+#
+#     The name of the binary itself ("pennsieve")
+#
+#   - ps_version : string =>
+#
+#     The version string of the release ("0.1.x")
+#
+#   - ps_executable : string =>
+#
+#     The absolute path to the Pennsieve binary, e.g
+#     /usr/local/opt/pennsieve/bin/${ps_release_name}
+#
+###############################################################################
+
+PS_HOME="$HOME/.pennsieve"
+PS_PATH="<%= ps_path %>"
+PS_EXECUTABLE="<%= ps_executable %>"
+
+# Create the Pennsieve home directory, if needed:
+if [ ! -d "$PS_HOME" ]; then
+	mkdir "$PS_HOME"
+fi
+
+INSTALL_LOG="$PS_HOME/install.log"
+
+echo "Install log: $INSTALL_LOG"
+
+echo "Installed $(date -u +"%Y-%m-%dT%H:%M:%SZ")" > $INSTALL_LOG
+echo "PS_HOME=$PS_HOME" >> $INSTALL_LOG
+echo "PS_PATH=<%= ps_path %>" >> $INSTALL_LOG
+echo "PS_RELEASE_NAME=<%= ps_release_name %>" >> $INSTALL_LOG
+echo "PS_VERSION=<%= ps_version %>" >> $INSTALL_LOG
+echo "PS_EXECUTABLE=<%= ps_executable %>" >> $INSTALL_LOG
+
+# Set the appropriate permissions:
+USER=$(who | awk '{print $1}')
+sudo chown -R $USER:$USER "$PS_HOME/"
+chmod -R a+rX "$PS_HOME"
+chmod 755 "$PS_PATH"
+
+# Create the cache directory:
+if [ ! -d "$PS_HOME/cache" ]; then
+	mkdir "$PS_HOME/cache"
+fi
+
+# Symlink $PS_EXECUTABLE to /usr/local/bin:
+if [ -d "/usr/local/bin" ]; then
+  sudo ln -s -f "$PS_EXECUTABLE" "/usr/local/bin/pennsieve"
+fi
---
+++ ci/unix/mac_post_install.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Note:
+#
+# This is an ERB template that is converted into a post-installation
+# script run by `fpm`. It is to be used with the `fpm` arguments
+# `--template-script` and `--template-value`.
+#
+# IT IS NOT MEANT TO BE RUN DIRECTLY!
+#
+# Expected variables:
+#
+#   - ps_path : string =>
+#
+#     The path to the Pennsieve installation, e.g. "/usr/local/opt/pennsieve",
+#     "C:\Program Files\pennsieve", etc.
+#
+#   - ps_release_name : string =>
+#
+#     The name of the binary itself ("pennsieve")
+#
+#   - ps_version : string =>
+#
+#     The version string of the release ("0.1.x")
+#
+#   - ps_executable : string =>
+#
+#     The absolute path to the Pennsieve binary, e.g
+#     /usr/local/opt/pennsieve/bin/${ps_release_name}
+#
+###############################################################################
+
+PS_HOME="$HOME/.pennsieve"
+PS_PATH="<%= ps_path %>"
+PS_EXECUTABLE="<%= ps_executable %>"
+
+# Create the Pennsieve home directory, if needed:
+if [ ! -d "$PS_HOME" ]; then
+	mkdir "$PS_HOME"
+fi
+
+INSTALL_LOG="$PS_HOME/install.log"
+
+echo "Install log: $INSTALL_LOG"
+
+echo "Installed $(date -u +"%Y-%m-%dT%H:%M:%SZ")" > $INSTALL_LOG
+echo "PS_HOME=$PS_HOME" >> $INSTALL_LOG
+echo "PS_PATH=<%= ps_path %>" >> $INSTALL_LOG
+echo "PS_RELEASE_NAME=<%= ps_release_name %>" >> $INSTALL_LOG
+echo "PS_VERSION=<%= ps_version %>" >> $INSTALL_LOG
+echo "PS_EXECUTABLE=<%= ps_executable %>" >> $INSTALL_LOG
+
+# Set the appropriate permissions:
+if [ -n "$USER" ]; then
+	sudo chown $USER:staff "$PS_HOME"
+	sudo chown -R $USER:staff "$PS_HOME"
+fi
+sudo chmod 755 "$PS_PATH"
+sudo chgrp staff "$PS_PATH"
+sudo chgrp -R staff "$PS_PATH"
+
+# Create the cache directory:
+if [ ! -d "$PS_HOME/cache" ]; then
+	mkdir "$PS_HOME/cache"
+fi
+
+# Symlink $PS_EXECUTABLE to /usr/local/bin:
+if [ -d "/usr/local/bin" ]; then
+  sudo ln -s -f "$PS_EXECUTABLE" "/usr/local/bin/pennsieve"
+fi
