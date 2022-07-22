e6ca28ab95e363d89a569a817699b4545b1f8f21
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2022-07-22 15:04:54 -0400

    fix test for windows

--- pkg/server/manifest.go
+++ pkg/server/manifest.go
@@ -326,7 +326,11 @@ func recordsFromPaths(paths []string, localBasePath string, targetBasePath strin
 		if err != nil {
 			log.Fatal("Cannot strip base-path.")
 		}
-		r2 := regexp.MustCompile(`(?P<Path>([^\/\\]*[\/\\])*)(?P<FileName>[^\.]*)?\.?(?P<Extension>.*)`)
+
+		// ensure path separator is slash
+		relPath = filepath.ToSlash(relPath)
+
+		r2 := regexp.MustCompile(`(?P<Path>([^\/]*\/)*)(?P<FileName>[^\.]*)?\.?(?P<Extension>.*)`)
 		pathParts := r2.FindStringSubmatch(relPath)
 
 		filePath := pathParts[r2.SubexpIndex("Path")]
