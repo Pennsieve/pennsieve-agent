54c8f32ff49dcab7f7ae3cd761af06e821a57a26
Edmore Moyo <etmoyo@gmail.com>
2024-03-12 00:35:15 -0400

    updates response

--- pkg/server/account.go
+++ pkg/server/account.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	api "github.com/pennsieve/pennsieve-agent/api/v1"
 	"github.com/pennsieve/pennsieve-agent/internal/aws"
@@ -29,9 +30,19 @@ func (s *server) Register(ctx context.Context, req *api.RegisterRequest) (*api.R
 		}
 		fmt.Println(awsRole)
 
-		return &api.RegisterResponse{Id: 1}, nil
+		return &api.RegisterResponse{
+			AccountId: extractAccountId(awsRole.Role.Arn)}, nil
 	default:
 		return nil, errors.New(fmt.Sprintf("Unsupported accountType: %s", accountType))
 	}
 
+}
+
+func extractAccountId(roleArn string) string {
+	parts := strings.Split(roleArn, ":")
+
+	if len(parts) < 6 {
+		return ""
+	}
+	return parts[4]
 }
