10d60b4469691c6d565c0eff287df5d8e95d183d
Edmore Moyo <etmoyo@gmail.com>
2024-04-02 19:23:45 -0400

    refactors

--- internal/aws/role_manager.go
+++ internal/aws/role_manager.go
@@ -25,24 +25,8 @@ func NewAWSRoleManager(pennsieveAccountId string, profile string) account.Regist
 func (r *AWSRoleManager) Create() ([]byte, error) {
 	roleName := fmt.Sprintf("ROLE-%s", r.AccountId)
 
-	cmd := exec.Command("aws", "--profile", r.Profile, "iam", "get-role", "--role-name", roleName)
-	var outb, errb bytes.Buffer
-	cmd.Stdout = &outb
-	cmd.Stderr = &errb
-	err := cmd.Run()
-
-	// return immediately if role exists
-	if err == nil && strings.Contains(outb.String(), roleName) {
-		log.Println("role exists")
-		return nil, errors.New("role exists")
-	}
-
-	log.Println("err: ", errb.String())
-	// check whether role does not exist
-	if strings.Contains(errb.String(), "cannot be found") {
-		log.Println("role does not exist creating ...")
-
-		trustPolicy := fmt.Sprintf(`{
+	// create trust policy
+	trustPolicy := fmt.Sprintf(`{
 			"Version": "2012-10-17",
 			"Statement": [
 				{
@@ -54,53 +38,52 @@ func (r *AWSRoleManager) Create() ([]byte, error) {
 				}
 			]
 		}`, r.AccountId)
+	trustPolicyFile := fmt.Sprintf("TRUST_POLICY_%s.json", r.AccountId)
 
-		trustPolicyFile := fmt.Sprintf("TRUST_POLICY_%s.json", r.AccountId)
-
-		err = os.WriteFile(fmt.Sprintf("%s/internal/aws/%s", projectpath.Root, trustPolicyFile),
-			[]byte(trustPolicy), 0644)
-		if err != nil {
-			log.Println("error writing data:", err)
-			return nil, err
-		}
-
-		// create role
-		cmd := exec.Command("aws",
-			"--profile", r.Profile,
-			"iam", "create-role",
-			"--role-name", roleName,
-			"--assume-role-policy-document", fmt.Sprintf("file://%s", trustPolicyFile))
-		var outb, errb bytes.Buffer
-		cmd.Stdout = &outb
-		cmd.Stderr = &errb
-		cmd.Dir = fmt.Sprintf("%s/internal/aws", projectpath.Root)
-		err = cmd.Run()
-		if err != nil {
-			return nil, errors.New(errb.String())
-		}
-
-		// attach inline permissions
-		permissionPolicyFile := "PERMISSION_POLICY.json"
-		policyName := fmt.Sprintf("POLICY-%s", r.AccountId)
-		permissionsCmd := exec.Command("aws",
-			"--profile", r.Profile,
-			"iam", "put-role-policy",
-			"--policy-name", policyName,
-			"--role-name", roleName,
-			"--policy-document", fmt.Sprintf("file://%s", permissionPolicyFile))
-		var poutb, perrb bytes.Buffer
-		permissionsCmd.Stdout = &poutb
-		permissionsCmd.Stderr = &perrb
-		permissionsCmd.Dir = fmt.Sprintf("%s/internal/aws", projectpath.Root)
-		err = permissionsCmd.Run()
-		if err != nil {
-			log.Println(perrb.String())
-			return nil, errors.New(perrb.String())
-		}
-
-		return outb.Bytes(), nil
+	err := os.WriteFile(fmt.Sprintf("%s/internal/aws/%s", projectpath.Root, trustPolicyFile),
+		[]byte(trustPolicy), 0644)
+	if err != nil {
+		log.Println("error writing data:", err)
+		return nil, err
 	}
 
-	// other errors - exit with error
-	return nil, errors.New(errb.String())
+	// create role
+	cmd := exec.Command("aws",
+		"--profile", r.Profile,
+		"iam", "create-role",
+		"--role-name", roleName,
+		"--assume-role-policy-document", fmt.Sprintf("file://%s", trustPolicyFile))
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
+	cmd.Dir = fmt.Sprintf("%s/internal/aws", projectpath.Root)
+	err = cmd.Run()
+	if err != nil {
+		log.Println(errb.String())
+		if strings.Contains(errb.String(), "EntityAlreadyExists") {
+			return nil, errors.New("role already exists")
+		}
+		return nil, errors.New(errb.String())
+	}
+
+	// attach inline permissions
+	permissionPolicyFile := "PERMISSION_POLICY.json"
+	policyName := fmt.Sprintf("POLICY-%s", r.AccountId)
+	permissionsCmd := exec.Command("aws",
+		"--profile", r.Profile,
+		"iam", "put-role-policy",
+		"--policy-name", policyName,
+		"--role-name", roleName,
+		"--policy-document", fmt.Sprintf("file://%s", permissionPolicyFile))
+	var poutb, perrb bytes.Buffer
+	permissionsCmd.Stdout = &poutb
+	permissionsCmd.Stderr = &perrb
+	permissionsCmd.Dir = fmt.Sprintf("%s/internal/aws", projectpath.Root)
+	err = permissionsCmd.Run()
+	if err != nil {
+		log.Println(perrb.String())
+		return nil, errors.New(perrb.String())
+	}
+
+	return outb.Bytes(), nil
 }
