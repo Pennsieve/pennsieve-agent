625f2c21a8ce2f77189777f0ee8d6259ccf4ee36
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2024-09-29 15:09:35 -0400

    try fix windowns paths

--- pkg/server/pull_test.go
+++ pkg/server/pull_test.go
@@ -8,24 +8,24 @@ import (
 
 func TestFindMappedDatasetRoot(t *testing.T) {
 
-	root, found, err := findMappedDatasetRoot("../../resources/test/pullTest/folder_1/folder_2")
+	root, found, err := findMappedDatasetRoot(path.Join("..", "..", "resources", "test", "pullTest", "folder_1", "folder_2"))
 	assert.NoError(t, err)
 	assert.True(t, found)
 	_, lastPathName := path.Split(root)
 	assert.Equal(t, "pullTest", lastPathName,
 		"Should find the manifest file in root folder when starting in subfolder in mapped dataset.")
 
-	root, found, err = findMappedDatasetRoot("../../resources/test/pullTest")
+	root, found, err = findMappedDatasetRoot(path.Join("..", "..", "resources", "test", "pullTest"))
 	assert.NoError(t, err)
 	assert.True(t, found,
 		"Should find the manifest file in root folder when starting at root")
 
-	root, found, err = findMappedDatasetRoot("../../resources")
+	root, found, err = findMappedDatasetRoot(path.Join("..", "..", "resources"))
 	assert.NoError(t, err)
 	assert.False(t, found,
 		"Should not find a root if not starting in mapped dataset")
 
-	root, found, err = findMappedDatasetRoot("../../resources/test/pullTest/folder_1/folder_2/file_4.txt")
+	root, found, err = findMappedDatasetRoot(path.Join("..", "..", "resources", "test", "pullTest", "folder_1", "folder_2", "file_4.txt"))
 	assert.NoError(t, err)
 	assert.True(t, found,
 		"Should find manifest when input is a file-name")
