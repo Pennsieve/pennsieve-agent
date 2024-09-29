aa2ceff3ec229b07654cf7d91827caf1ebc62ed9
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2024-09-29 13:31:18 -0400

    fix test

--- pkg/shared/shared_test.go
+++ pkg/shared/shared_test.go
@@ -1,25 +1,25 @@
 package shared
 
 import (
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestCrCCheck(t *testing.T) {
 
-    // Check CRC
-    nrBytes := 1048576 // 1MB
-    crc, err := GetFileCrc32("../../resources/test/.pennsieve/manifest.json", nrBytes)
-    assert.NoError(t, err)
-    assert.Equal(t, uint32(0xdb58346b), crc)
+	// Check CRC
+	nrBytes := 1048576 // 1MB
+	crc, err := GetFileCrc32("../../resources/test/pullTest/.pennsieve/manifest.json", nrBytes)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(0xa1dc9897), crc)
 
-    // Check that crc is idempotent
-    crc, err = GetFileCrc32("../../resources/test/.pennsieve/manifest.json", nrBytes)
-    assert.NoError(t, err)
-    assert.Equal(t, uint32(0xdb58346b), crc)
+	// Check that crc is idempotent
+	crc, err = GetFileCrc32("../../resources/test/pulltest/.pennsieve/manifest.json", nrBytes)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(0xa1dc9897), crc)
 
-    // Check crc with maxbuffer that is smaller than file
-    crc, err = GetFileCrc32("../../resources/test/.pennsieve/manifest.json", 50)
-    assert.NoError(t, err)
-    assert.Equal(t, uint32(0x46c16dc9), crc)
+	// Check crc with maxbuffer that is smaller than file
+	crc, err = GetFileCrc32("../../resources/test/pullTest/.pennsieve/manifest.json", 50)
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(0x3842f4f6), crc)
 }
