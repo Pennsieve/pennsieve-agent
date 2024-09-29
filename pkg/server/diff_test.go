d3d157a32353f294fa775dead716681f3f861895
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2024-09-29 14:18:45 -0400

    try fix windowns paths

--- pkg/server/diff_test.go
+++ pkg/server/diff_test.go
@@ -6,14 +6,15 @@ import (
 	"github.com/pennsieve/pennsieve-agent/pkg/shared"
 	models "github.com/pennsieve/pennsieve-go-core/pkg/models/workspaceManifest"
 	"github.com/stretchr/testify/assert"
+	"path"
 	"testing"
 )
 
 func TestCompareManifest(t *testing.T) {
-	datasetRoot := "../../resources/test/mapDataset"
+	datasetRoot := path.Join("..", "..", "resources", "test", "mapDataset")
 	files, _ := createFolderManifest(datasetRoot)
 
-	manifest, err := shared.ReadWorkspaceManifest("../../resources/test/mapDataset/.pennsieve/manifest.json")
+	manifest, err := shared.ReadWorkspaceManifest(path.Join(datasetRoot, ".pennsieve", "manifest.json"))
 	assert.NoError(t, err)
 
 	result, err := compareManifestToFolder(datasetRoot, manifest.Files, files)
@@ -24,45 +25,45 @@ func TestCompareManifest(t *testing.T) {
 	count := 0
 	for _, f := range result {
 		switch f.FilePath {
-		case "folder_1/file_10_added.txt":
+		case path.Join("folder_1", "file_10_added.txt"):
 			assert.Equal(t, api.PackageStatus_ADDED, f.Type)
 			assert.Equal(t, f.New.FileName, "file_10_added.txt")
 			count++
-		case "folder_1/file_4_renamed.txt":
+		case path.Join("folder_1", "file_4_renamed.txt"):
 			assert.Equal(t, api.PackageStatus_RENAMED, f.Type)
 			assert.Equal(t, f.New.FileName, "file_4_renamed.txt")
 			assert.Equal(t, f.Old.FileName, "file_4.txt")
 			count++
-		case "folder_1/folder_1_1/file_5_moved.txt":
+		case path.Join("folder_1", "folder_1_1", "file_5_moved.txt"):
 			assert.Equal(t, api.PackageStatus_MOVED, f.Type)
 			assert.Equal(t, f.New.Path, "folder_1/folder_1_1")
 			assert.Equal(t, f.Old.Path, "folder_1")
 			count++
-		case "folder_2/file_7_downloaded_renamed.txt":
+		case path.Join("folder_2", "file_7_downloaded_renamed.txt"):
 			assert.Equal(t, api.PackageStatus_RENAMED, f.Type)
 			assert.Equal(t, f.New.FileName, "file_7_downloaded_renamed.txt")
 			assert.Equal(t, f.Old.FileName, "file_7_downloaded.txt")
 			count++
-		case "folder_2/folder_2_1/file_12_downloaded_moved_renamed.txt":
+		case path.Join("folder_2", "folder_2_1", "file_12_downloaded_moved_renamed.txt"):
 			assert.Equal(t, api.PackageStatus_MOVED_RENAMED, f.Type)
 			assert.Equal(t, f.New.FileName, "file_12_downloaded_moved_renamed.txt")
 			assert.Equal(t, f.Old.FileName, "file_12_downloaded.txt")
 			assert.Equal(t, f.New.Path, "folder_2/folder_2_1")
 			assert.Equal(t, f.Old.Path, "folder_2")
 			count++
-		case "folder_2/folder_2_1/file_13_moved_renamed.txt":
+		case path.Join("folder_2", "folder_2_1", "file_13_moved_renamed.txt"):
 			assert.Equal(t, api.PackageStatus_MOVED_RENAMED, f.Type)
 			assert.Equal(t, f.New.FileName, "file_13_moved_renamed.txt")
 			assert.Equal(t, f.Old.FileName, "file_13.txt")
 			assert.Equal(t, f.New.Path, "folder_2/folder_2_1")
 			assert.Equal(t, f.Old.Path, "folder_2")
 			count++
-		case "folder_2/folder_2_1/file_8_downloaded_moved.txt":
+		case path.Join("folder_2", "folder_2_1", "file_8_downloaded_moved.txt"):
 			assert.Equal(t, api.PackageStatus_MOVED, f.Type)
 			assert.Equal(t, f.New.Path, "folder_2/folder_2_1")
 			assert.Equal(t, f.Old.Path, "folder_2")
 			count++
-		case "folder_2/file_9_downloaded_changed.txt":
+		case path.Join("folder_2", "file_9_downloaded_changed.txt"):
 			assert.Equal(t, api.PackageStatus_CHANGED, f.Type)
 			assert.Equal(t, f.Changed.from.Size, models.NullInt{NullInt64: sql.NullInt64{Valid: true, Int64: 35}})
 			assert.Equal(t, f.Changed.Size, int64(44))
